f9c54975c649f33da09e7f6b0f670e88f9afe390
Roger Zhang <zhangqinyuan2013@live.cn>
2018-04-27 16:15:56 +0800

    Add Header Title Section and make it responsive for mobile view

--- src/extensions/header/HeaderApplicationCustomizer.ts
+++ src/extensions/header/HeaderApplicationCustomizer.ts
@@ -35,7 +35,7 @@ export default class HeaderApplicationCustomizer
   }
 
   private _renderHeaderContent(): void {
-    console.log('HelloWorldApplicationCustomizer._renderPlaceHolders()');
+    console.log('HeaderApplicationCustomizer._renderPlaceHolders()');
     console.log('Available placeholders: ',
       this.context.placeholderProvider.placeholderNames.map(name => PlaceholderName[name]).join(', '));
     // Handling the top placeholder
@@ -53,11 +53,100 @@ export default class HeaderApplicationCustomizer
       if (this._headerContent.domElement) {
         require('./HeaderAppCustomizer.scss');
         this._headerContent.domElement.innerHTML = `
-                    <div class="app">
-                      <div class="ms-bgColor-themeDark ms-fontColor-white top">
-                        <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> Welcome
-                      </div>
-                    </div>`;
+        <div class="fuse-header">
+        <div class="fuse-header-titleArea">
+          <div class="fuse-header-logoSlidesContainer">
+            <div class="fuse-siteLogoContainer">
+                <a class="fuse-siteLogo-link" id="ctl00_onetidProjectPropertyTitleGraphic" href="/sites/fusedemonorthstarprime/">
+                    <img alt="Northstar" src="/sites/fusedemonorthstarprime/SiteAssets/untitled.png">
+                </a>
+            </div>
+            <div class="fuse-slides-container">
+                <div class="fuse-weather-slides" style="position: relative; overflow: hidden;">
+                    <div style="position: absolute; top: 60px; left: 0px; display: none; z-index: 8; opacity: 1;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/01n.png">
+                        <span class="fuse-weather-data">4 °C / 39 °F 11:52pm
+                            <br>New York, US
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 60px; left: 0px; display: none; z-index: 8; opacity: 1; width: 205px; height: 47px;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/02n.png">
+                        <span class="fuse-weather-data">14 °C / 58 °F 10:52pm
+                            <br>Austin, US
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 60px; left: 0px; display: none; z-index: 8; opacity: 1; width: 205px; height: 47px;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/10n.png">
+                        <span class="fuse-weather-data">7 °C / 45 °F 4:52am
+                            <br>London, GB
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 60px; left: 0px; display: none; z-index: 8; opacity: 1; width: 205px; height: 47px;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/01n.png">
+                        <span class="fuse-weather-data">7 °C / 44 °F 5:52am
+                            <br>Paris, FR
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 60px; left: 0px; display: none; z-index: 8; opacity: 1; width: 205px; height: 47px;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/01n.png">
+                        <span class="fuse-weather-data">17 °C / 62 °F 6:52am
+                            <br>Istanbul, TR
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 60px; left: 0px; display: none; z-index: 8; opacity: 1; width: 205px; height: 47px;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/01d.png">
+                        <span class="fuse-weather-data">25 °C / 77 °F 7:52am
+                            <br>Dubai, AE
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 0px; left: 0px; display: block; z-index: 9; opacity: 1; width: 205px; height: 47px;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/50d.png">
+                        <span class="fuse-weather-data">30 °C / 86 °F 9:22am
+                            <br>Delhi, IN
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                    <div style="position: absolute; top: 0px; left: 0px; display: none; z-index: 1;">
+                        <img alt="Weather Image" src="https://openweathermap.org/img/w/01d.png">
+                        <span class="fuse-weather-data">25 °C / 77 °F 11:52am
+                            <br>Shanghai, CN
+                            <br>
+                            <span id="fuse-text-openweathermap" style="font-size:10px">OpenWeatherMap</span>
+                        </span>
+                    </div>
+                </div>
+                <div class="fuse-stock-slides" style="overflow: hidden;">
+                    <div style="position: absolute; top: 32px; left: 0px; display: none; z-index: 4; opacity: 1; width: 145px; height: 31px;">ADS ($0.14) (-0.07 %) $203.96</div>
+                    <div style="position: absolute; top: 0px; left: 0px; display: block; z-index: 5; opacity: 1; width: 145px; height: 31px;">MSFT ($0.33) (-0.35 %) $94.26</div>
+                    <div style="position: absolute; top: 32px; left: 0px; display: none; z-index: 4; opacity: 1; width: 145px; height: 31px;">GE $0.01 (0.07 %) $14.40</div>
+                    <div style="position: absolute; top: 32px; left: 0px; display: none; z-index: 4; opacity: 1; width: 145px; height: 31px;">BA ($0.08) (-0.02 %) $342.79</div>
+                </div>
+            </div>
+          </div>
+            <div class="fuse-search-container">
+                <div class="fuse-searchBoxControl">
+                    <input title="Search..." type="search" placeholder="Search..."/>
+                    <a role="button">
+                      <img alt="Search" src="/_layouts/15/images/searchresultui.png?rev=44#ThemeKey=searchresultui">
+                    </a>
+                </div>     
+            </div>
+        </div>
+    </div>
+      `;
       }
     }
   }
